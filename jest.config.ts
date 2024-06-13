fad959f82f4f5e17ddfb6a1226209d5791c44e89
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-06-13 13:10:41 +0700

    trying to ci cd to aws amplify

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default config;
